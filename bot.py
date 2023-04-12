9c458972d9949b017934455fca56ba3e35208682
xmd79 <34522857+xmd79@users.noreply.github.com>
2023-04-13 02:44:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -108,7 +108,7 @@ def long_condition(candles, stop_loss, take_profit):
     Determines if a long position should be opened based on the sinewave talib from 1min with close price data from 3min tf.
     Returns True if a long position should be opened, False otherwise.
     """
-    print("Running short_condition()...")
+    print("Running long_condition()...")
     tf_3m_close = np.array(candles['3m'])[:, 3]
     sw = talib.SINWMA(tf_3m_close, timeperiod=5)
     sw_diff = np.diff(sw)
@@ -116,60 +116,36 @@ def long_condition(candles, stop_loss, take_profit):
     min_sw = np.min(sw)
     exit_point = (max_sw + min_sw) / 2 + (max_sw - min_sw) / 4  # Exit point is 3/4 of the way up the sine wave
     entry_point = (max_sw + min_sw) / 2 - (max_sw - min_sw) / 4  # Entry point is 3/4 of the way down the sine wave
-    stop_loss_price = candles['1m'][-1]['close'] * (1 - stop_loss)
-    take_profit_price = candles['1m'][-1]['close'] * (1 + take_profit)
-    
-    print("Sine Wave:", sw)
-    print("Sine Wave Differences:", sw_diff)
-    print("Max Sine Wave Value:", max_sw)
-    print("Min Sine Wave Value:", min_sw)
-    print("Entry Point:", entry_point)
-    print("Exit Point:", exit_point)
-    print("Stop loss percentage:", stop_loss)
-    print("Take profit percentage:", take_profit)
-    print("Stop loss price:", stop_loss_price)
-    print("Take profit price:", take_profit_price)
-
-    if (sw[-1] < entry_point) and (sw_diff[-1] < sw_diff[-2]):
-        print("Long position condition met")
+    stop_loss_price = candles['1m'][-1]['close'] * (1 - stop_loss)  # calculate stop loss price
+    take_profit_price = candles['1m'][-1]['close'] * (1 + take_profit)  # calculate take profit price
+    if sw[-2] < entry_point and sw[-1] > entry_point and sw[-1] > sw[-2] and sw[-1] > sw_diff[-1] and sw_diff[-1] > 0 and sw_diff[-2] > 0:
+        print("Long condition met.")
         return True, stop_loss_price, take_profit_price
     else:
-        print("Long position condition not met")
-        return False, None, None
+        print("Long condition not met.")
+        return False, 0, 0
 
-def short_condition(candles, stop_loss, take_profit):
+def long_condition(candles, stop_loss, take_profit):
     """
-    Determines if a short position should be opened based on the sinewave talib from 1min with close price data from 3min tf.
-    Returns True if a short position should be opened, False otherwise.
+    Determines if a long position should be opened based on the sinewave talib from 1min with close price data from 3min tf.
+    Returns True if a long position should be opened, False otherwise.
     """
-    print("Running short_condition()...")
+    print("Running long_condition()...")
     tf_3m_close = np.array(candles['3m'])[:, 3]
     sw = talib.SINWMA(tf_3m_close, timeperiod=5)
     sw_diff = np.diff(sw)
     max_sw = np.max(sw)
     min_sw = np.min(sw)
-    entry_point = (max_sw + min_sw) / 2 + (max_sw - min_sw) / 4  # Entry point is 3/4 of the way up the sine wave
-    exit_point = (max_sw + min_sw) / 2 - (max_sw - min_sw) / 4  # Exit point is 3/4 of the way down the sine wave
-    stop_loss_price = candles['1m'][-1]['close'] * (1 + stop_loss)
-    take_profit_price = candles['1m'][-1]['close'] * (1 - take_profit)
-    print("Sine wave values:", sw)
-    print("Sine wave differences:", sw_diff)
-    print("Max sine wave value:", max_sw)
-    print("Min sine wave value:", min_sw)
-    print("Entry point for short position:", entry_point)
-    print("Exit point for short position:", exit_point)
-    print("Stop loss percentage:", stop_loss)
-    print("Take profit percentage:", take_profit)
-    print("Current candle close price:", candles['1m'][-1]['close'])
-    print("Stop loss price:", stop_loss_price)
-    print("Take profit price:", take_profit_price)
-
-    if (sw[-1] > entry_point) and (sw_diff[-1] > sw_diff[-2]):
-        print("Short condition met!")
+    exit_point = (max_sw + min_sw) / 2 + (max_sw - min_sw) / 4  # Exit point is 3/4 of the way up the sine wave
+    entry_point = (max_sw + min_sw) / 2 - (max_sw - min_sw) / 4  # Entry point is 3/4 of the way down the sine wave
+    stop_loss_price = candles['1m'][-1]['close'] * (1 - stop_loss)  # calculate stop loss price
+    take_profit_price = candles['1m'][-1]['close'] * (1 + take_profit)  # calculate take profit price
+    if sw[-2] < entry_point and sw[-1] > entry_point and sw[-1] > sw[-2] and sw[-1] > sw_diff[-1] and sw_diff[-1] > 0 and sw_diff[-2] > 0:
+        print("Long condition met.")
         return True, stop_loss_price, take_profit_price
     else:
-        print("Short condition not met.")
-        return False, None, None
+        print("Long condition not met.")
+        return False, 0, 0
 
 def cancel_all_positions(symbol):
     """
@@ -210,51 +186,123 @@ def get_sinewave(ticker, interval, period):
     return sinewave
 
 def main():
-    open_positions = client.futures_position_information()
-    side = get_trade_side()
+    # initialize variables
+    position_size = 100
+    entry_price = 1.2000
+    stop_loss = 0.0050
+    take_profit = 0.0100
+    open_positions = []
+    is_reversal_key_point = False
     
-    # Check for stop loss or take profit
-    else:
-        closed_positions = []
-        for position in open_positions:
-            if position['positionAmt'] != '0' and position['positionSide'] == side and position['symbol'] == TRADE_SYMBOL:
-                current_price = float(client.futures_symbol_ticker(symbol=TRADE_SYMBOL)['price'])
-                stop_loss_price = float(position['entryPrice']) * (1 - STOP_LOSS_THRESHOLD)
-                take_profit_price = float(position['entryPrice']) * (1 + TAKE_PROFIT_THRESHOLD)
-                if current_price <= stop_loss_price:
-                    print(f"{trade_type.upper()} TRADE - STOP LOSS HIT - Closing position...")
-                    order = client.futures_create_order(
-                        symbol=TRADE_SYMBOL,
-                        side=Client.SIDE_SELL if side == Client.SIDE_BUY else Client.SIDE_BUY,
-                        type=Client.ORDER_TYPE_MARKET,
-                        quantity=abs(float(position['positionAmt'])),
-                        reduceOnly=True
-                    )
-                    closed_positions.append(position['orderId'])
-                elif current_price >= take_profit_price:
-                    print(f"{trade_type.upper()} TRADE - TAKE PROFIT HIT - Closing position...")
-                    order = client.futures_create_order(
-                        symbol=TRADE_SYMBOL,
-                        side=Client.SIDE_SELL if side == Client.SIDE_BUY else Client.SIDE_BUY,
-                        type=Client.ORDER_TYPE_MARKET,
-                        quantity=abs(float(position['positionAmt'])),
-                        reduceOnly=True
-                    )
-                    closed_positions.append(position['orderId'])
-
-        # Close all positions in case of reversal of sine
-        if reversal_of_sine():
-            print(f"REVERSAL OF SINE - CLOSING ALL POSITIONS...")
+    while True:
+        # get current market price
+        current_price = get_market_price()
+        
+        # calculate momentum of sine function
+        momentum = calculate_sine_momentum(current_price)
+        
+        if len(open_positions) == 0:
+            # if there are no open positions, check for entry conditions
+            if momentum > 0 and current_price < entry_price - stop_loss:
+                # enter long position
+                open_positions.append(('long', position_size, current_price))
+                print(f'Entered long position at {current_price}')
+            elif momentum < 0 and current_price > entry_price + stop_loss:
+                # enter short position
+                open_positions.append(('short', position_size, current_price))
+                print(f'Entered short position at {current_price}')
+            elif is_reversal_key_point:
+                # reload script from beginning
+                is_reversal_key_point = False
+                print('Reloaded script from beginning')
+                
+        else:
+            # if there are open positions, check for exit conditions
             for position in open_positions:
-                if position['positionAmt'] != '0' and position['symbol'] == TRADE_SYMBOL:
-                    order = client.futures_create_order(
-                        symbol=TRADE_SYMBOL,
-                        side=Client.SIDE_SELL if position['positionSide'] == Client.SIDE_BUY else Client.SIDE_BUY,
-                        type=Client.ORDER_TYPE_MARKET,
-                        quantity=abs(float(position['positionAmt'])),
-                        reduceOnly=True
-                    )
-                    closed_positions.append(position['orderId'])
+                side = position[0]
+                if side == 'long':
+                    if current_price > position[2] + take_profit:
+                        # close long position
+                        close_price = current_price
+                        pnl = (close_price - position[2]) * position[1]
+                        print(f'Closed long position at {close_price} for a profit of {pnl}')
+                        open_positions.remove(position)
+                    elif current_price < position[2] - stop_loss:
+                        # close long position
+                        close_price = current_price
+                        pnl = (close_price - position[2]) * position[1]
+                        print(f'Closed long position at {close_price} for a loss of {pnl}')
+                        open_positions.remove(position)
+                        # init new short trade with reversed position size
+                        position_size *= -1
+                        open_positions.append(('short', position_size, current_price))
+                        print(f'Entered new short position at {current_price}')
+                elif side == 'short':
+                    if current_price < position[2] - take_profit:
+                        # close short position
+                        close_price = current_price
+                        pnl = (position[2] - close_price) * position[1]
+                        print(f'Closed short position at {close_price} for a profit of {pnl}')
+                        open_positions.remove(position)
+                    elif current_price > position[2] + stop_loss:
+                        # close short position
+                        close_price = current_price
+                        pnl = (position[2] - close_price) * position[1]
+                        print(f'Closed short position at {close_price} for a loss of {pnl}')
+                        open_positions.remove(position)
+                        # init new long trade with reversed position size
+                        position_size *= -1
+                        open_positions.append(('long', position_size, current_price))
+                        print(f'Entered new long position at {current_price}')
 
-        if closed_positions:
-            update_trade_log(closed_positions)
+            if is_reversal_key_point:
+                # reload script from beginning
+                is_reversal_key_point = False
+                print('Reloaded script from beginning')
+
+            elif any(abs(current_price - position[2]) > take_profit for position in open_positions):
+                # hit take profit momentum, close all positions and end trade
+                total_pnl = 0
+                for position in open_positions:
+                    if current_price > position[2]: # long position
+                        pnl = (current_price - position[2]) * position[1]
+                        print(f"Closed {position[1]} contracts at {current_price} for a profit of {pnl}")
+                    else: # short position
+                        pnl = (position[2] - current_price) * position[1]
+                        print(f"Closed {position[1]} contracts at {current_price} for a profit of {pnl}")
+                    total_pnl += pnl
+                print(f"Total PnL for this trade: {total_pnl}")
+                open_positions = []
+                print('Exited trade at take profit momentum')
+                # reload script from beginning
+                is_reversal_key_point = False
+                print('Reloaded script from beginning')
+
+            elif any(abs(current_price - position[2]) > stop_loss for position in open_positions):
+                # hit stop loss, close all positions and initiate new trade with opposite side
+                total_pnl = 0
+                for position in open_positions:
+                    if current_price > position[2]:  # long position
+                        pnl = (current_price - position[2]) * position[1]
+                        print(f"Closed {position[1]} contracts at {current_price} for a loss of {pnl}")
+                    else:  # short position
+                        pnl = (position[2] - current_price) * position[1]
+                        print(f"Closed {position[1]} contracts at {current_price} for a loss of {pnl}")
+                    total_pnl += pnl
+                print(f"Total PnL for this trade: {total_pnl}")
+                open_positions = []
+                print('Exited trade at stop loss')
+                # initiate new trade with opposite side
+                entry_price = current_price
+                if open_positions[0][0] == 'long':
+                    trade_side = 'short'
+                    position_size *= -1
+                else:
+                    trade_side = 'long'
+                    position_size *= -1
+                open_positions.append((trade_side, position_size, entry_price))
+                print(f"Entered new trade at {entry_price} with {position_size} contracts, {trade_side} side")
+                # reload script from beginning
+                is_reversal_key_point = False
+                print('Reloaded script from beginning')
+                
